9b64c735fcc4351be1e1585fc91ec025b0127abc
Shaw Summa <shawsumma@gmail.com>
2022-05-26 22:18:23 -0400

    git hates me

--- Makefile
+++ Makefile
@@ -3,21 +3,34 @@ OPT=-O3
 8OBJS=cpp.o debug.o dict.o gen.o lex.o vector.o parse.o buffer.o map.o \
      error.o path.o file.o set.o encoding.o
 VOBJS=asm.o gc.o jump.o reguse.o int/run.o int/comp.o
-OBJS=$(8OBJS) $(VOBJS:%=vm/vm/%)
 
-REAL_OPT=$(GCC_OPT) $(OPT)
+REAL_OPT=-fno-ssa-phiopt $(OPT)
 
 default: all
 
-all: minivm-cc
+all: bin/vmcc bin/vmasm
 
-minivm-cc: 8cc.h main.o $(OBJS)
-	$(CC) $(REAL_OPT) -o $(@) main.o $(OBJS) $(LDFLAGS) -lm
+bin/vmcc: src/8cc.h main.o $(8OBJS) $(VOBJS)
+	@mkdir -p bin
+	$(CC) $(REAL_OPT) -o $(@) obj/cc/main.o $(8OBJS:%=obj/cc/%) $(VOBJS:%=obj/vm/%) $(LDFLAGS) -lm
 
-$(OBJS) main.o: $(@:%.o=%.c) 8cc.h keyword.inc
-	$(CC) $(REAL_OPT) -o $(@) -c $(@:%.o=%.c) $(CFLAGS)
+bin/vmasm: $(VOBJS) obj/vm/main.o
+	@mkdir -p bin
+	$(CC) $(REAL_OPT) -o $(@) vm/util/main.c $(VOBJS:%=obj/vm/%) -lm
+
+obj/vm/main.o: 
+	@mkdir -p obj/vm
+	$(CC) $(REAL_OPT) -o obj/vm/main.o -c vm/util/main.c $(CFLAGS)
+
+$(VOBJS): $(@:%.o=vm/vm/%.c) src/8cc.h src/keyword.inc
+	@mkdir -p obj/vm/int
+	$(CC) $(REAL_OPT) -o obj/vm/$(@) -c $(@:%.o=vm/vm/%.c) $(CFLAGS)
+
+$(8OBJS) main.o: $(@:%.o=src/%.c) src/8cc.h src/keyword.inc
+	@mkdir -p obj/cc
+	$(CC) $(REAL_OPT) -o obj/cc/$(@) -c $(@:%.o=src/%.c) $(CFLAGS)
 
 clean: .dummy
-	rm -f 8cc $(OBJS)
+	rm -r bin obj
 
 .dummy:
--- src/main.c
+++ src/main.c
@@ -8,38 +8,29 @@
 #include <unistd.h>
 #include "8cc.h"
 #define VM_NO_FILE 1
-#include "vm/vm/asm.h"
+#include "../vm/vm/asm.h"
+
+enum {
+    OUTPUT_RUN,
+    OUTPUT_ASM,
+    OUTPUT_BC,
+};
 
 static Vector *infiles = &EMPTY_VECTOR;
-static char *asmfile;
+static char *outfile;
+static int outtype = OUTPUT_RUN;
+static char *rtsrc = "rt";
 static Buffer *cppdefs;
 
 static void usage(int exitcode) {
     fprintf(exitcode ? stderr : stdout,
-            "Usage: minivm-cc <file>\n\n"
+            "Usage: vmcc <file>\n\n"
             "\n"
+            "  -n                dont include runtime\n"
+            "  -r                runtime directory\n"
             "  -h                print this help\n"
             "\n");
     exit(exitcode);
-}
-
-static void parseopt(int argc, char **argv) {
-    cppdefs = make_buffer();
-    int i = 1;
-    while (i < argc) {
-        char *arg = argv[i++];
-        if (arg[0] == '-') {
-            switch(arg[1]) {
-            case 'h':
-                usage(0);
-                break;
-            default:
-                usage(1);
-            }
-        } else {
-            vec_push(infiles, arg);
-        }
-    }
 }
 
 static char *filetype(char *name) {
@@ -49,6 +40,46 @@ static char *filetype(char *name) {
     }
     return &name[where];
 }
+
+static void parseopt(int argc, char **argv) {
+    cppdefs = make_buffer();
+    int i = 1;
+    while (i < argc) {
+        char *arg = argv[i++];
+        if (arg[0] == '-') {
+            switch(arg[1]) {
+            case 'n': {
+                rtsrc = NULL;
+                break;
+            }
+            case 'r': {
+                rtsrc = argv[i++];
+                break;
+            }
+            case 'o': {
+                outfile = argv[i++];
+                char *ext = filetype(outfile);   
+                if (!strcmp(ext, ".vasm")) {
+                    outtype = OUTPUT_ASM;
+                } else if (!strcmp(ext, ".bc")) {
+                    outtype = OUTPUT_BC;
+                }
+                break;
+            }
+            case 'h':
+                printf("%s\n", arg);
+                usage(0);
+                break;
+            default:
+                printf("%s\n", arg);
+                usage(1);
+            }
+        } else {
+            vec_push(infiles, arg);
+        }
+    }
+}
+
 
 char *infile;
 char *get_base_file(void) {
@@ -60,12 +91,14 @@ int main(int argc, char **argv) {
     setbuf(stdout, NULL);
     parseopt(argc, argv);
     Vector *asmbufs = &EMPTY_VECTOR;
-    vec_push(infiles, "rt/src/stdio.c");
-    vec_push(infiles, "rt/src/bitop.c");
-    vec_push(infiles, "rt/src/start.c");
-    vec_push(infiles, "rt/src/tinyalloc.c");
-    vec_push(infiles, "rt/src/mem.asm");
-    add_include_path("rt/include");
+    if (rtsrc != NULL) {
+        vec_push(infiles, format("%s/src/stdio.c", rtsrc));
+        vec_push(infiles, format("%s/src/bitop.c", rtsrc));
+        vec_push(infiles, format("%s/src/start.c", rtsrc));
+        vec_push(infiles, format("%s/src/tinyalloc.c", rtsrc));
+        vec_push(infiles, format("%s/src/mem.asm", rtsrc));
+        add_include_path(format("%s/include", rtsrc));
+    }
     for (int i = 0; i < vec_len(infiles); i++) {
         infile = vec_get(infiles, i);
         char *ext = filetype(infile);
@@ -81,7 +114,7 @@ int main(int argc, char **argv) {
                 Node *v = vec_get(toplevels, i);
                 emit_toplevel(v);
             }
-        } else if (!strcmp(ext, ".vasm") || !strcmp(ext, ".asm") || !strcmp(ext, ".vs") || !strcmp(ext, ".s")) {
+        } else if (!strcmp(ext, ".vasm")) {
             Buffer *asmbuf = make_buffer();
             FILE *file = fopen(infile, "r");
             if (file == NULL) {
@@ -101,15 +134,22 @@ int main(int argc, char **argv) {
     for (int i = 0; i < vec_len(asmbufs); i++) {
         buf_printf(src, "\n%s\n", vec_get(asmbufs, i));
     }
-    {
-        FILE *out = fopen("a.vasm", "w");
+    if (outtype == OUTPUT_ASM) {
+        FILE *out = fopen(outfile, "w");
         fwrite(src->body, sizeof(char), src->len, out);
         fclose(out);
+        return 0;
     }
     // printf("%s\n", src->body);
     vm_asm_buf_t buf = vm_asm(src->body);
     if (buf.nops == 0) {
         return 1;
+    }
+    if (outtype == OUTPUT_BC) {
+        FILE *out = fopen(outfile, "wb");
+        fwrite(buf.ops, sizeof(vm_opcode_t), buf.nops, out);
+        fclose(out);
+        return 0;
     }
     int res = vm_run_arch_int(buf.nops, buf.ops);
     if (res != 0) {
